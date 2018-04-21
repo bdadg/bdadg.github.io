d2271ac4884fc01fd55968a6db25019bb9d76711
kimeualexis <kimeualexis@gmail.com>
2018-04-21 13:20:29 +0300

    details

---
+++ js/custom.js
@@ -0,0 +1,119 @@
+$(document).ready(function() {
+    $("#myBtn").click(function() {
+        $("#Modal").modal();
+    });
+});
+
+$(document).ready(function() {
+    $("#myBtn1").click(function() {
+        $("#Modal1").modal();
+    });
+});
+
+$(document).ready(function() {
+    $("#myBtn2").click(function() {
+        $("#Modal2").modal();
+    });
+});
+
+$(document).ready(function() {
+    $("#myBtn3").click(function() {
+        $("#Modal3").modal();
+    });
+});
+
+$(document).ready(function() {
+    $("#myBtn4").click(function() {
+        $("#Modal4").modal();
+    });
+});
+
+$(document).ready(function() {
+    $("#myBtn5").click(function() {
+        $("#Modal5").modal();
+    });
+});
+
+$(document).ready(function() {
+    $("#myBtn6").click(function() {
+        $("#Modal6").modal();
+    });
+});
+
+$(document).ready(function() {
+    $("#myBtn7").click(function() {
+        $("#Modal7").modal();
+    });
+});
+
+$(document).ready(function() {
+    $("#myBtn8").click(function() {
+        $("#Modal8").modal();
+    });
+});
+
+$(document).ready(function() {
+    $("#myBtn9").click(function() {
+        $("#Modal9").modal();
+    });
+});
+
+$(document).ready(function() {
+    $("#myBtn10").click(function() {
+        $("#Modal10").modal();
+    });
+});
+
+$(document).ready(function() {
+    $("#myBtn11").click(function() {
+        $("#Modal11").modal();
+    });
+});
+
+$(document).ready(function() {
+    $("#myBtn12").click(function() {
+        $("#Modal12").modal();
+    });
+});
+
+$(document).ready(function() {
+    $("#myBtn13").click(function() {
+        $("#Modal13").modal();
+    });
+});
+
+$(document).ready(function() {
+    $("#myBtn14").click(function() {
+        $("#Modal14").modal();
+    });
+});
+
+$(document).ready(function() {
+    $("#myBtn15").click(function() {
+        $("#Modal15").modal();
+    });
+});
+
+$(document).ready(function() {
+    $("#myBtn16").click(function() {
+        $("#Modal16").modal();
+    });
+});
+
+$(document).ready(function() {
+    $("#myBtn17").click(function() {
+        $("#Modal17").modal();
+    });
+});
+
+$(document).ready(function() {
+    $("#myBtn18").click(function() {
+        $("#Modal18").modal();
+    });
+});
+
+$(document).ready(function() {
+    $("#myBtn19").click(function() {
+        $("#Modal19").modal();
+    });
+});
